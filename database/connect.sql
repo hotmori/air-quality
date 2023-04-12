11445b5c7627d5df281a1ea1ffe40b8b5cc9d771
Dmitriy <dmitriy.ischenko@dotmatics.com>
2023-04-12 11:41:59 +0300

    Adding metabase

--- database/connect.sql
+++ database/connect.sql
@@ -7,3 +7,5 @@ psql --username=repl_user  --host=postgresql-slave --dbname=my_database
 repl_password
 
 connect to metabase db:
+psql --username=mb_user --host=metabase-postgres-db --dbname=mb_database
+mb_password
