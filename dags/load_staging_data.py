53a0ff72818f34790ed5d546b5d22fb3d6c8c10f
Dmitriy <dmitriy.ischenko@dotmatics.com>
2023-04-13 16:52:48 +0300

    DB scripts

--- dags/load_staging_data.py
+++ dags/load_staging_data.py
@@ -8,39 +8,33 @@ import requests, json
 
 def finalize():
     print("All is done")
+        
 
 def get_cities():
-    request = "select * from staging.cities"
+    request = "select city_id, longitude, latitude from staging.cities"
     pg_hook = PostgresHook(postgre_conn_id = "postgres_default")
     connection = pg_hook.get_conn()
     cursor = connection.cursor()
     cursor.execute(request)
     cities = cursor.fetchall()
+    city_dict = {}
+
     for city in cities:
-        print("City: {0} - {1}".format(city[0], city[1]))
-    return cities
+        city_dict[city[0]] = {"longitude": city[1], "latitude": city[2]}
 
-def get_openweather_data():
-    cities = get_cities()
-    url = 'http://api.openweathermap.org/data/2.5/air_pollution?lat=59.9343&lon=30.3351'
+    return city_dict
+
+def get_city_air_data(latitude, longitude):
+    base_url = 'http://api.openweathermap.org/data/2.5/air_pollution?'
     key = Variable.get("openweather_k")
-    request_url = f'{url}&appid={key}'
-    print("request_url", request_url)
-
-    x = requests.get(request_url)
-    status_code = x.status_code
-    print("status_code: ",status_code)
+    request_url = f'{base_url}lat={latitude}&lon={longitude}&appid={key}'
+    response = requests.get(request_url)
+    status_code = response.status_code
     if status_code != 200:
-        raise ValueError('Openweather API connection error status code:', status_code, " message: ", x.message)
+        raise ValueError('Openweather API connection error status code:', status_code, " message: ", response)
 
-    json_data = x.json()
+    json_data = response.json()
 
-    #json_data = json.loads('{"coord": {"lon": 30.3351, "lat": 59.9343}, "list": [{"main": {"aqi": 3}, "components": {"co": 894.55, "no": 81.36, "no2": 49.35, "o3": 11.27, "so2": 101.09, "pm2_5": 17.8, "pm10": 25.09, "nh3": 1.44}, "dt": 1681204196}]}')
-
-    print("json_data: ", json_data)
-
-    latitude = json_data["coord"]["lat"]
-    longitude = json_data["coord"]["lon"]
     metric_list = json_data["list"]
     aqi = metric_list[0]["main"]["aqi"]
     components =  metric_list[0]["components"]
@@ -54,36 +48,75 @@ def get_openweather_data():
     component_nh3 = components["nh3"]
     ux_timestamp = metric_list[0]["dt"]
 
+    city_air_data = {
+                   "ux_timestamp": ux_timestamp,
+                   "aqi": aqi,
+                   "component_co": component_co,
+                   "component_no" : component_no,
+                   "component_no2" : component_no2,
+                   "component_o3" : component_o3,
+                   "component_so2" : component_so2,
+                   "component_pm2_5" : component_pm2_5,
+                   "component_pm10" : component_pm10,
+                   "component_nh3" : component_nh3}
 
-    print("latitude: ", latitude)
-    print("longitude: ", longitude)
+    return city_air_data
 
-    print("metric_listx: ", metric_list)
-    print("aqi: ", aqi)
-    print("component_co: ", component_co)
-    print("component_nh3: ", component_nh3)
-    print("dt: ", ux_timestamp)
-    print(datetime.utcfromtimestamp(ux_timestamp).strftime('%Y-%m-%d %H:%M:%S'))
- 
-    openweather_result = {"ux_timestamp": ux_timestamp,
-                          "latitude": latitude,
-                          "longitude": longitude,
-                          "aqi": aqi,
-                          "component_co": component_co,
-                          "component_no" : component_no,
-                          "component_no2" : component_no2,
-                          "component_o3" : component_o3,
-                          "component_so2" : component_so2,
-                          "component_pm2_5" : component_pm2_5,
-                          "component_pm10" : component_pm10,
-                          "component_nh3" : component_nh3
-                         }
+def get_openweather_data():
+    cities = get_cities()
+    cities_air_data = {}
+    for city in cities:
+        print("city_id: ", city, "data: ", cities[city], "lon:", cities[city]["longitude"])
+        city_air_data = get_city_air_data(latitude = cities[city]["latitude"], longitude=cities[city]["longitude"] )
+        cities_air_data[city] = city_air_data
+        
+    print("cities_air_data", cities_air_data)
 
-    return openweather_result
+
+    sql_inserts = generate_inserts(cities_air_data)
+    return sql_inserts
+
+def generate_inserts(cities_air_data):
+    sql_ins = """insert into staging.cities_air(city_id, \
+                                                ts, \
+                                                aqi, \
+                                                co, \
+                                                no, \
+                                                no2, \
+                                                o3, \
+                                                pm2_5, \
+                                                pm10, \
+                                                nh3 \
+                                                ) """
+    sql_vals = ""
+    for i, city_air_data in enumerate(cities_air_data):
+
+        sql_vals += (',' if i > 0 else '') + f'({city_air_data},  \
+                       to_timestamp({cities_air_data[city_air_data]["ux_timestamp"]}), \
+                       {cities_air_data[city_air_data]["aqi"]}, \
+                       {cities_air_data[city_air_data]["component_co"]}, \
+                       {cities_air_data[city_air_data]["component_no"]}, \
+                       {cities_air_data[city_air_data]["component_no2"]}, \
+                       {cities_air_data[city_air_data]["component_o3"]}, \
+                       {cities_air_data[city_air_data]["component_pm2_5"]},\
+                       {cities_air_data[city_air_data]["component_pm10"]},\
+                       {cities_air_data[city_air_data]["component_nh3"]}\
+                       )\n'
+    
+    result_sql = f'{sql_ins} values {sql_vals};'
+    result_sql = " ".join(result_sql.split())
+    print ("result_sql: ", result_sql)
+
+
+    pg_hook = PostgresHook(postgre_conn_id = "postgres_default")
+    connection = pg_hook.get_conn()
+    cursor = connection.cursor()
+    cursor.execute(result_sql)
+    return result_sql
 
 with DAG(dag_id="load_staging_data",
          start_date=datetime(2021,1,1),
-         schedule_interval="*/5 * * * *",
+         schedule_interval="5 * * * *",
          catchup=False) as dag:
 
     task_get_openweather_data = PythonOperator(
@@ -94,36 +127,8 @@ with DAG(dag_id="load_staging_data",
                                                 postgres_conn_id="postgres_default",
                                                 sql = "SELECT 1 x;")
 
-    task_save_data = PostgresOperator(task_id = "save_data_postgres_db",
-                                      postgres_conn_id="postgres_default",
-                                      sql = """insert into staging.cities_air(city_id, \
-                                                                        ts, \
-                                                                        aqi, \
-                                                                        co, \
-                                                                        no, \
-                                                                        no2, \
-                                                                        o3, \
-                                                                        pm2_5, \
-                                                                        pm10, \
-                                                                        nh3 \
-                                                                        )
-                                               values('1', \
-                                                      to_timestamp('{{ ti.xcom_pull(key='return_value', task_ids='get_openweather_data')['ux_timestamp'] }}'), \
-                                                                   '{{ ti.xcom_pull(key='return_value', task_ids='get_openweather_data')['aqi'] }}', \
-                                                                   '{{ ti.xcom_pull(key='return_value', task_ids='get_openweather_data')['component_co'] }}', \
-                                                                   '{{ ti.xcom_pull(key='return_value', task_ids='get_openweather_data')['component_no'] }}', \
-                                                                   '{{ ti.xcom_pull(key='return_value', task_ids='get_openweather_data')['component_no2'] }}', \
-                                                                   '{{ ti.xcom_pull(key='return_value', task_ids='get_openweather_data')['component_o3'] }}', \
-                                                                   '{{ ti.xcom_pull(key='return_value', task_ids='get_openweather_data')['component_pm2_5'] }}', \
-                                                                   '{{ ti.xcom_pull(key='return_value', task_ids='get_openweather_data')['component_pm10'] }}', \
-                                                                   '{{ ti.xcom_pull(key='return_value', task_ids='get_openweather_data')['component_nh3'] }}' \
-                                                                   );
-                                            """,
-                                      params = {},
-                                      autocommit = True)
-
     task_finalize = PythonOperator(
         task_id="finalize",
         python_callable=finalize)
 
-task_get_openweather_data >> task_connect_postgres_db >> task_save_data
+task_get_openweather_data >> task_connect_postgres_db #>> task_save_data
