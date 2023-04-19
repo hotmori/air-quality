86f3404dd2396f0a26d60966529bc686f8d1b611
Dmitriy <dmitriy.ischenko@dotmatics.com>
2023-04-19 18:00:29 +0300

    Reporting layer

--- database/reporting/views/vcities_air.sql
+++ database/reporting/views/vcities_air.sql
@@ -1,3 +1,17 @@
 create or replace view vcities_air
 as
-select 1 x;
+select ca.city_id,
+       ca.city_name,
+       ca.country_name,
+       ca.longitude,
+       ca.latitude,
+       ca.ts_hour timestamp,
+       ca.aqi,
+       ca.co,
+       ca.no,
+       ca.no2,
+       ca.o3,
+       ca.pm2_5,
+       ca.pm10,
+       ca.nh3
+  from transform.cities_air ca;
